1281c4b70b42bf64a076dba555167e42ed2672cc
Dhanush <dhanushg860@gmail.com>
2025-08-15 23:34:51 +0530

    Remove compiled JS files from repository

--- src/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { model, Schema } from "mongoose";
-mongoose.connect("mongodb+srv://viktor2142315:bStjVGrijZD64c7h@cluster0.jqiky.mongodb.net/");
-const UserSchema = new Schema({
-    username: { type: String, unique: true },
-    password: String
-});
-export const UserModel = model("User", UserSchema);
-//# sourceMappingURL=db.js.map
